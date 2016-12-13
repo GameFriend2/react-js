3293bcf17c06916ad1ed167b26de7840912b9d96
TaiKi Kim <ktk6176@naver.com>
2016-12-13 22:42:06 +0900

    create method  구현 완료
    ContactCreateComponent 에서
    handleClick method 와  handleChange method를 통해
    상태변화 인지 및 입력받은 값을 메소로 넘기고 입력받은 메소드는
     Contact Component에 있는 handleCreate에서 새로운 유저를 생성

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -27,11 +27,11 @@ export default class Contact extends React.Component{
                 name : 'David',
                 phone : '010-0000-0004'
             }]
-        }
+        };
         this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
-        // this.handleCreate =this.handleCreate(this);
-        // this.handleRemove = this.handleRemove(this);
+        this.handleCreate =this.handleCreate.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
         // this.handleEdit =this.handleEdit(this);
     }
 
@@ -46,21 +46,21 @@ export default class Contact extends React.Component{
             selectedkey:key
         })
     }
-    //
-    // handleCreate(contact) {
-    //     this.setState({
-    //         contactData: update(this.state.contactData, { $push: [contact]})
-    //     })
-    // }
-    //
-    // handleRemove(e){
-    //     this.setState({
-    //         contactData: update(this.state.contactData,
-    //             { $splice : [[this.state.selectedkey, 1]] }
-    //             ),
-    //             selectedkey: -1
-    //         });
-    // }
+
+    handleCreate(contact) {
+        this.setState({
+            contactData: update(this.state.contactData, { $push : [contact]  })
+        })
+    }
+
+    handleRemove(e){
+        this.setState({
+            contactData: update(this.state.contactData,
+                { $splice : [[this.state.selectedkey, 1]] }
+                ),
+                selectedkey: -1
+            });
+    }
     // handleEdit(name, phone){
     //     this.setState({
     //         contactData: update(this.state.contactData,
@@ -106,7 +106,9 @@ export default class Contact extends React.Component{
                 <ContactDetails
                     isSelected={this.state.selectedkey != -1}
                     contact={this.state.contactData[this.state.selectedkey]}/>
-                <ContactCreate />
+                <ContactCreate
+                    onCreate={this.handleCreate}
+                />
             </div>
         )
     }
